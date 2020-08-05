d959cfa4d0ed49bcfa028d16dd8f51e58b769feb
tanvee <balharatanvee@gmail.com>
2020-08-05 23:57:15 +0530

    snapchat filter challenge completed

--- snapchat filter challenge/snapchat_filter.py
+++ snapchat filter challenge/snapchat_filter.py
@@ -1,7 +1,10 @@
 import cv2
+import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-# frame = cv2.imread('./test/Before.png')
-frame = cv2.imread('Jamie_Before.jpg')
+frame = cv2.imread('./test/Before.png')
+# frame = cv2.imread('Jamie_Before.jpg')
 # cv2.imshow('Image', frame)
 
 eyes_cascade = cv2.CascadeClassifier('./third-party/frontalEyes35x16.xml')
@@ -17,7 +20,7 @@ eyes = sorted(eyes, key = lambda f : f[2]*f[3])
 nose = sorted(nose, key = lambda f : f[2]*f[3])
     
 eye_x, eye_y, eye_w, eye_h = eyes[-1]
-# cv2.rectangle(frame, (eye_x, eye_y), (eye_x + eye_w, eye_y + eye_h), (0, 0, 255), 5)
+# cv2.rectangle(frame, (eye_x, eye_y), (eye_x + eye_w, eye_y + eye_h), (0, 0, 255), 2)
 glasses = cv2.resize(glasses, (eye_w, eye_h))
 print(glasses.shape, eye_w, eye_h, eye_x, eye_y)
 for i in range (eye_w) :
@@ -27,7 +30,7 @@ for i in range (eye_w) :
 # cv2.imshow('Glasses', glasses)
 
 nose_x, nose_y, nose_w, nose_h = nose[-1]
-cv2.rectangle(frame, (nose_x, nose_y), (nose_x + nose_w, nose_y + nose_h), (255, 0, 0), 5)
+# cv2.rectangle(frame, (nose_x, nose_y), (nose_x + nose_w, nose_y + nose_h), (255, 0, 0), 2)
 mustache = cv2.resize(mustache, (nose_w, nose_h))
 print(mustache.shape, nose_w, nose_h, nose_x, nose_y)
 for i in range (nose_w) :
@@ -37,6 +40,20 @@ for i in range (nose_w) :
 # cv2.imshow('Mustache', mustache)
 
 cv2.imshow('Eyes and Nose', frame)
+# cv2.imwrite('answer.png', frame)
 
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print(frame.shape)
+frame = frame.reshape((-1, 3))
+print(frame.shape)
+
+df = pd.DataFrame(frame, dtype = 'uint8', columns = ['Channel 1', 'Channel 2', 'Channel 3'])
+df.to_csv('ans_csv.csv', index = False)
+
+df2 = pd.read_csv('ans_csv.csv')
+data = df2.values
+cv2.imshow('blabla', data.reshape(485, 377, 3))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
